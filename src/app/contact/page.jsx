fb4c3eb707db650f53f531016df063c0efa73a23
Jorge Matos <jisapjisap@gmail.com>
2024-03-03 18:07:02 +0100

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,6 +3,9 @@
 import { motion } from "framer-motion"
 
 const ContactPage = () => {
+
+  const text = "Say hello"
+
   return (
     <motion.div
       className="h-full"
@@ -10,7 +13,34 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Contact
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <motion.div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </motion.div>
+        </div>
+        {/* FORM CONTAINER */}
+        <form
+          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+        >
+
+        </form>
+      </div>
     </motion.div>
   )
 }
