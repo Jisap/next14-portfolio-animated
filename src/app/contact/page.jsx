4c76b9284c74a502dd60fe2a897a6b090eee073c
Jorge Matos <jisapjisap@gmail.com>
2024-03-03 18:15:43 +0100

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,15 @@
 "use client"
 
 import { motion } from "framer-motion"
+import { useRef, useState } from "react";
 
 const ContactPage = () => {
 
   const text = "Say hello"
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const form = useRef();
+
 
   return (
     <motion.div
@@ -16,7 +21,7 @@ const ContactPage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
-          <motion.div>
+          <div>
             {text.split("").map((letter, index) => (
               <motion.span
                 key={index}
@@ -32,13 +37,38 @@ const ContactPage = () => {
               </motion.span>
             ))}
             😊
-          </motion.div>
+          </div>
         </div>
         {/* FORM CONTAINER */}
         <form
           className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
         >
-
+          <span>Dear Jisap Dev,</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="user_message"
+          />
+          <span>My mail address is:</span>
+          <input
+            name="user_email"
+            type="text"
+            className="bg-transparent border-b-2 border-b-black outline-none"
+          />
+          <span>Regards</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
         </form>
       </div>
     </motion.div>
