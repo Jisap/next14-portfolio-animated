d3388906daeaf5379b8fb7103eb77719a1ed3fac
Jorge Matos <jisapjisap@gmail.com>
2024-03-05 18:19:43 +0100

    loader

--- loader.js
+++ loader.js
@@ -4,5 +4,5 @@ export default function myImageLoader({
   src, width, quality
 }){
   if(src.startsWith('https://images.pexels.com')) return src
-  return `https://https://portfolionext14.netlify.app/${src}?w={width}&q={quality || 75}`
+  return `https://portfolionext14.netlify.app/${src}?w={width}&q={quality || 75}`
 }
